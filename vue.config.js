c61ae5248d7cff54907968f59d626bce979726f2
Vitor Hugo <vto.hugo67@gmail.com>
2025-11-11 22:37:31 -0300

    feat: Add GitHub Actions deploy workflow and set publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/JourneyCalendar/" : "/",
   transpileDependencies: true,
   lintOnSave: false, // Disable ESLint during serve to avoid version conflicts
   chainWebpack: (config) => {
